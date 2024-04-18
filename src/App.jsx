6378d4adb2557174e591488f7922eb2de6e38730
TheoAt <theo.atlani@gmail.com>
2024-04-18 15:19:36 +0200

    starting project by initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Tooltip from './components/Tooltip';
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Tooltip in ReactJS</h1>
+      <Tooltip />
+    </div>
+  );
+}
+
+export default App;
